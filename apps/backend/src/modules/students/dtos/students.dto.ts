cc5b3c4fce7c960ee78e2ac0663672eb95b20182
BODIAN Bacary Bruno <bacarybruno@gmail.com>
2022-04-06 01:36:54 +0200

    docs(students): improve swagger documentation

--- apps/backend/src/modules/students/dtos/students.dto.ts
+++ apps/backend/src/modules/students/dtos/students.dto.ts
@@ -2,11 +2,39 @@ import { ApiProperty } from '@nestjs/swagger';
 import {
   IsDateString,
   IsEmail,
+  IsNumber,
   IsPhoneNumber,
   IsString,
 } from 'class-validator';
+import { Student } from '../entities/student.entity';
 
 export class CreateStudentDto {
+  @ApiProperty()
+  @IsString()
+  firstName: string;
+
+  @ApiProperty()
+  @IsString()
+  lastName: string;
+
+  @ApiProperty()
+  @IsDateString()
+  birthDate: Date;
+
+  @ApiProperty()
+  @IsEmail()
+  email: string;
+
+  @ApiProperty()
+  @IsPhoneNumber()
+  phone: string;
+}
+
+export class StudentDto implements Student {
+  @ApiProperty()
+  @IsNumber()
+  id: number;
+
   @ApiProperty()
   @IsString()
   firstName: string;
--- apps/backend/src/modules/students/students.controller.ts
+++ apps/backend/src/modules/students/students.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
-import { CreateStudentDto } from './dtos/students.dto';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { CreateStudentDto, StudentDto } from './dtos/students.dto';
 import { StudentsService } from './students.service';
 
 @Controller('students')
@@ -9,11 +9,13 @@ export class StudentsController {
   constructor(private readonly studentsService: StudentsService) {}
 
   @Get()
+  @ApiOkResponse({ type: StudentDto, isArray: true })
   getAllStudents() {
     return this.studentsService.getAllStudents();
   }
 
   @Post()
+  @ApiCreatedResponse({ type: StudentDto })
   createStudent(@Body() createStudentDto: CreateStudentDto) {
     return this.studentsService.createStudent(createStudentDto);
   }
