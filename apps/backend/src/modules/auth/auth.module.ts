06f600a1663bf52bec795934acaed1a883280b5e
BODIAN Bacary Bruno <bacarybruno@gmail.com>
2022-04-08 02:16:43 +0200

    chore: use firebase module

--- apps/backend/src/modules/auth/auth.module.ts
+++ apps/backend/src/modules/auth/auth.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
+import { FirebaseModule } from '../firebase/firebase.module';
 import { FirebaseAuthStrategy } from './firebase-auth.strategy';
 import { RolesGuard } from './roles-auth.guard';
 
 @Module({
-  imports: [PassportModule.register({ defaultStrategy: 'firebase-jwt' })],
+  imports: [PassportModule.register({ defaultStrategy: 'firebase-jwt' }), FirebaseModule],
   providers: [FirebaseAuthStrategy, RolesGuard],
   exports: [PassportModule, RolesGuard],
 })
--- apps/backend/src/modules/auth/firebase-auth.strategy.ts
+++ apps/backend/src/modules/auth/firebase-auth.strategy.ts
@@ -1,39 +1,21 @@
 import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import * as firebase from 'firebase-admin';
 import { Strategy, ExtractJwt } from 'passport-firebase-jwt';
-import { FirebaseConfig } from '@prepa-sn/shared/interfaces';
+import { FirebaseService } from '../firebase/firebase.service';
 
 @Injectable()
 export class FirebaseAuthStrategy extends PassportStrategy(Strategy) {
   private logger = new Logger('FirebaseAuthStrategy');
-  private firebaseParams: FirebaseConfig;
-  private firebaseApp = null;
 
-  constructor() {
+  constructor(private readonly firebaseService: FirebaseService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    });
-    this.firebaseParams = {
-      type: process.env.FIREBASE_TYPE,
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      clientId: process.env.FIREBASE_CLIENT_ID,
-      authUri: process.env.FIREBASE_AUTH_URI,
-      tokenUri: process.env.FIREBASE_TOKEN_URI,
-      authProviderX509CertUrl: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-      clientC509CertUrl: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-    };
-    this.firebaseApp = firebase.initializeApp({
-      credential: firebase.credential.cert(this.firebaseParams),
     });
   }
 
   async validate(token: string) {
     try {
-      const user = await this.firebaseApp.auth().verifyIdToken(token, true);
+      const user = await this.firebaseService.verifyToken(token);
       if (!user) throw new UnauthorizedException("User doesn't exist");
       return user;
     } catch (error) {
