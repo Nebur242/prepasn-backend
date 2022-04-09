e3b3a676a584e8cd3e5296e8c62adefc8c05245d
BODIAN Bacary Bruno <bacarybruno@gmail.com>
2022-04-09 16:48:38 +0200

    refactor: atomic firebase config imports

--- apps/backend/src/modules/firebase/firebase.service.ts
+++ apps/backend/src/modules/firebase/firebase.service.ts
@@ -1,6 +1,6 @@
-import { FirebaseConfig } from '@prepa-sn/shared/interfaces';
 import { Injectable } from '@nestjs/common';
-import * as firebase from 'firebase-admin';
+import { initializeApp, cert } from 'firebase-admin/app';
+import { auth } from 'firebase-admin';
 import { HttpService } from '@nestjs/axios';
 import { lastValueFrom, map } from 'rxjs';
 import { Role } from '@prepa-sn/shared/enums';
@@ -8,28 +8,25 @@ import { configService } from '../../common/services/config.service';
 
 @Injectable()
 export class FirebaseService {
-  private firebaseParams: FirebaseConfig;
-  private firebaseApp: firebase.app.App = null;
-
   constructor(private readonly httpService: HttpService) {
-    this.firebaseParams = configService.getFirebaseConfig();
-    this.firebaseApp = firebase.initializeApp({
-      credential: firebase.credential.cert(this.firebaseParams),
+    initializeApp({
+      credential: cert(configService.getFirebaseConfig()),
     });
   }
 
   verifyToken(token: string, checkRevoked = false) {
-    return this.firebaseApp
-      .auth()
-      .verifyIdToken(token?.replace('Bearer', '')?.trim(), checkRevoked);
+    return auth().verifyIdToken(
+      token?.replace('Bearer', '')?.trim(),
+      checkRevoked
+    );
   }
 
   createUser(email: string, password: string) {
-    return this.firebaseApp.auth().createUser({ email, password });
+    return auth().createUser({ email, password });
   }
 
   setRoles(uid: string, roles: Role[]) {
-    return this.firebaseApp.auth().setCustomUserClaims(uid, { roles });
+    return auth().setCustomUserClaims(uid, { roles });
   }
 
   login(email: string, password: string) {
