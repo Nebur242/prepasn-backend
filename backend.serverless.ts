b2f1ad4f03562ac96fc9dab773e8568ca8f764b9
BODIAN Bacary Bruno <bacarybruno@gmail.com>
2022-04-12 02:22:32 +0200

    refactor(serverless): use .ts file instead of .yml

---
+++ backend.serverless.ts
@@ -0,0 +1,55 @@
+import type { Serverless } from 'serverless/aws';
+import { parse } from 'dotenv';
+import { readFileSync } from 'fs';
+
+const app = 'backend';
+const appWorkspacePath = require('./workspace.json').projects[app];
+
+function parseEnv(path: string) {
+  try {
+    const envContents = readFileSync(path);
+    return parse(envContents);
+  } catch (e) {}
+}
+
+function getDotenvVariables() {
+  return {
+    ...parseEnv(`${__dirname}/.env`),
+    ...parseEnv(`${__dirname}/.local.env`),
+    ...parseEnv(`${__dirname}/.env.local`),
+    ...parseEnv(`${appWorkspacePath}/.env`),
+    ...parseEnv(`${appWorkspacePath}/.local.env`),
+    ...parseEnv(`${appWorkspacePath}/.env.local`),
+  };
+}
+
+const serverlessConfig: Serverless = {
+  service: app,
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs14.x',
+  },
+  functions: {
+    backend: {
+      handler: `dist/${appWorkspacePath}/main.handler`,
+      environment: getDotenvVariables(),
+      events: [
+        {
+          http: {
+            method: 'ANY',
+            path: '/',
+          },
+        },
+        {
+          http: {
+            method: 'ANY',
+            path: '{proxy+}',
+          },
+        },
+      ],
+    },
+  },
+  plugins: ['serverless-offline'],
+};
+
+module.exports = serverlessConfig;
