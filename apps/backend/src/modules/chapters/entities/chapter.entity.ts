8150260844b81d8c98b65bdc8c7065bd74156423
BODIAN Bacary Bruno <bacarybruno@gmail.com>
2022-04-19 18:34:56 +0200

    chore: make title non unique

--- apps/backend/src/modules/chapters/entities/chapter.entity.ts
+++ apps/backend/src/modules/chapters/entities/chapter.entity.ts
@@ -1,12 +1,9 @@
 import { BaseContent } from '../../../common/entities/base-content.entity';
-import { Column, Entity, JoinTable, OneToOne } from 'typeorm';
+import { Entity, JoinTable, OneToOne } from 'typeorm';
 import { Course } from '../../courses/entities/course.entity';
 
 @Entity()
 export class Chapter extends BaseContent {
-  @Column({ nullable: false, unique: true })
-  override title: string;
-
   //has one and only course
   @OneToOne(() => Course, (course) => course.chapters)
   @JoinTable()
--- apps/backend/src/modules/courses/entities/course.entity.ts
+++ apps/backend/src/modules/courses/entities/course.entity.ts
@@ -1,13 +1,10 @@
 import { BaseContent } from '../../../common/entities/base-content.entity';
-import { Column, Entity, JoinTable, ManyToMany, OneToMany } from 'typeorm';
+import { Entity, JoinTable, ManyToMany, OneToMany } from 'typeorm';
 import { Chapter } from '../../chapters/entities/chapter.entity';
 import { Grade } from '../../grades/entities/grade.entity';
 
 @Entity()
 export class Course extends BaseContent {
-  @Column({ unique: true })
-  override title: string;
-
   //has many grades
   @ManyToMany(() => Grade, (grade) => grade.courses)
   @JoinTable()
