2e9e746b656487977a1a5ae50466008ec2d32938
BODIAN Bacary Bruno <bacarybruno@gmail.com>
2022-05-04 22:10:16 +0200

    chore: trigger build

--- apps/hls/src/serverless.ts
+++ apps/hls/src/serverless.ts
@@ -3,7 +3,7 @@ import { handler } from './app/lambda';
 import { AWS_EXECUTION_ENV } from './app/constants';
 
 if (AWS_EXECUTION_ENV.includes('AWS_ECS')) {
-  // ffmpeg service running on ECS
+  // hls service running on ECS
   bootstrap();
 } else {
   // lambda service exposing the handler
