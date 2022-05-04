3258f75cc2b51eaeaa375271bba3ede990494ead
BODIAN Bacary Bruno <bacarybruno@gmail.com>
2022-05-04 23:02:37 +0200

    chore: run build

--- apps/hls/src/serverless.ts
+++ apps/hls/src/serverless.ts
@@ -3,7 +3,7 @@ import { handler } from './app/lambda';
 import { AWS_EXECUTION_ENV } from './app/constants';
 
 if (AWS_EXECUTION_ENV.includes('AWS_ECS')) {
-  // hls service running on ECS
+  // hls service running on AWS ECS
   bootstrap();
 } else {
   // lambda service exposing the handler
