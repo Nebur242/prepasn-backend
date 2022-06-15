fe56b20148b31f1c32a681fb980e530b7c39513c
Ruben Nzaou <rm.nzaou.etudes@gmail.com>
2022-06-15 23:40:06 +0100

    feat: feat: added some properties to exercises

--- apps/backend/src/modules/exercises/dto/create-exercise.dto.ts
+++ apps/backend/src/modules/exercises/dto/create-exercise.dto.ts
@@ -1,9 +1,21 @@
 import { CreateBaseContentDto } from '@prepa-sn/backend/common/dtos/create-base-content.dto';
-import { IsDefined, IsNumber } from 'class-validator';
+import { IsDefined, IsEnum, IsNumber } from 'class-validator';
 import { Chapter } from '../../chapters/entities/chapter.entity';
-
+import { displayType, exerciseType, level } from '../entities/exercise.entity';
 export class CreateExerciseDto extends CreateBaseContentDto {
   @IsDefined()
   @IsNumber()
   chapter: Chapter['id'];
+
+  @IsDefined()
+  @IsEnum(exerciseType)
+  type: exerciseType;
+
+  @IsDefined()
+  @IsEnum(level)
+  level: level;
+
+  @IsDefined()
+  @IsEnum(displayType)
+  display: displayType;
 }
--- apps/backend/src/modules/exercises/entities/exercise.entity.ts
+++ apps/backend/src/modules/exercises/entities/exercise.entity.ts
@@ -1,8 +1,28 @@
-import { Entity, ManyToOne, OneToMany } from 'typeorm';
+import { Column, Entity, ManyToOne, OneToMany } from 'typeorm';
 import { BaseContent } from '@prepa-sn/backend/common/entities/base-content.entity';
 import { Chapter } from '../../chapters/entities/chapter.entity';
 import { Question } from '../../questions/entities/question.entity';
 
+export enum exerciseType {
+  MULTIPLE_CHOICE = 'multiple_choice',
+  TRUE_FALSE = 'true_false',
+  FILL_IN_THE_BLANK = 'fill_in_the_blank',
+  MATCHING = 'matching',
+  ORDERING = 'ordering',
+  GROUPING = 'grouping',
+}
+
+export enum level {
+  EASY = 'easy',
+  MEDIUM = 'medium',
+  HARD = 'hard',
+}
+
+export enum displayType {
+  TEXT = 'text',
+  IMAGE = 'image',
+  VIDEO = 'video',
+}
 @Entity()
 export class Exercise extends BaseContent {
   @ManyToOne(() => Chapter, (chapter) => chapter.exercises)
@@ -10,4 +30,25 @@ export class Exercise extends BaseContent {
 
   @OneToMany(() => Question, (question) => question.exercise)
   questions?: Question[];
+
+  @Column({
+    type: 'enum',
+    enum: exerciseType,
+    default: exerciseType.MULTIPLE_CHOICE,
+  })
+  type: exerciseType;
+
+  @Column({
+    type: 'enum',
+    enum: level,
+    default: level.EASY,
+  })
+  level: level;
+
+  @Column({
+    type: 'enum',
+    enum: displayType,
+    default: displayType.TEXT,
+  })
+  display: displayType;
 }
