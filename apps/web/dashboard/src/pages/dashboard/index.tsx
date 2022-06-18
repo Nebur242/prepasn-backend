b6a430d24b19e3d1263bd37c5491e182442c6164
Ruben Merson Nzaou <rm.nzaou.etudes@gmail.com>
2022-06-18 14:17:38 +0100

    fix: quick fix

--- apps/web/dashboard/src/pages/dashboard/index.tsx
+++ apps/web/dashboard/src/pages/dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import * as routes from '../../config/routes.config';
 import { Route } from '../../config/routes.config';
 import {
